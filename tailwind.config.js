941e1242cd8d5df9b70833d25f0931aeabb38e0a
Jheyele Raquel <jheyele.xavier@atos.net>
2022-06-26 14:53:45 -0300

    Create new screen and updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        blur: 'url(/src/assets/blur-backgroud.png)'
+      },
       fontFamily: {
         sans: 'Roboto, sans-serif'
       },
